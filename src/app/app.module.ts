a871c651fca3fb2ab22eae4f6102a955f92fd431
Hardik <2019mcb1220@iitrpr.ac.in>
2021-06-08 21:09:12 +0530

    sorting component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SortingVisualiserComponent } from './sorting-visualiser/sorting-visualiser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortingVisualiserComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sorting-visualiser/sorting-visualiser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting-visualiser',
+  templateUrl: './sorting-visualiser.component.html',
+  styleUrls: ['./sorting-visualiser.component.css']
+})
+export class SortingVisualiserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
