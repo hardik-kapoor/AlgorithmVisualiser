e435670801e9ef0045fb9f13b536ae4339035968
Hardik <2019mcb1220@iitrpr.ac.in>
2021-06-11 17:22:19 +0530

    jump search and linear search done

--- src/app/searching-visualiser/searching-visualiser.component.ts
+++ src/app/searching-visualiser/searching-visualiser.component.ts
@@ -16,7 +16,6 @@ export class SearchingVisualiserComponent implements OnInit {
   arr:number[]=[];
   max_val:number=-1;
   dur:number=0;
-  size:number=this.arr.length;
   isFound:number[]=[];
   isChecking:number[]=[];
   isChecked:number[]=[];
@@ -31,13 +30,13 @@ export class SearchingVisualiserComponent implements OnInit {
   currentColor(ind:number)
   {
     if(this.isFound.includes(ind))
-      return {"background-color": '#ADD8E6', "width": String(Math.min(33,(650/this.size)))+"px" , "color": '#343a40'};
+      return {"background-color": '#ADD8E6', "width": String(Math.min(33,(650/this.arr.length)))+"px" , "color": '#343a40'};
     else if(this.isChecking.includes(ind))
-      return {"background-color":'#dc3545' , "width": String(Math.min(33,(650/this.size)))+"px" , "color": 'white'};
+      return {"background-color":'#dc3545' , "width": String(Math.min(33,(650/this.arr.length)))+"px" , "color": 'white'};
     else if(this.isChecked.includes(ind))
-      return {"background-color":'#76ba1b' , "width": String(Math.min(33,(650/this.size)))+"px", "color": 'white'};
+      return {"background-color":'#76ba1b' , "width": String(Math.min(33,(650/this.arr.length)))+"px", "color": 'white'};
     else
-      return {"background-color": '#343a40' , "width": String(Math.min(33,(650/this.size)))+"px" , "color": 'white'};
+      return {"background-color": '#343a40' , "width": String(Math.min(33,(650/this.arr.length)))+"px" , "color": 'white'};
   }
 
   createRandomArray(length,isSortReq)
@@ -55,10 +54,55 @@ export class SearchingVisualiserComponent implements OnInit {
   //fun1
     async linearSearch(num:number)
     {
+      this.reset();
       for(let i=0;i<this.arr.length;i++)
       {
+        this.isChecking.push(i);
+        await new Promise(resolve => setTimeout(resolve, this.dur));
+        if(this.arr[i]===num)
+        {
+          this.isFound.push(i);
+          await new Promise(resolve => setTimeout(resolve, this.dur));
+          break;
+        }
+        this.isChecked.push(i);
+        this.isChecking=[];
+        await new Promise(resolve => setTimeout(resolve, this.dur));
       }
     }
+
+    async jumpSearch(num:number)
+    {
+      this.reset();
+      let jump=Math.floor(Math.sqrt(this.arr.length));
+      let mx=this.arr.length-1;
+      for(let i=0;i<this.arr.length;i+=jump)
+      {
+        this.isChecking=[];
+        this.isChecking.push(i);
+        await new Promise(resolve => setTimeout(resolve, this.dur));
+        this.isChecking=[];
+        this.isChecked.push(i);
+        await new Promise(resolve => setTimeout(resolve, this.dur));
+        if(this.arr[i]>=num)
+        {
+          mx=i;
+          break;
+        }
+      }
+      for(let i=mx;i>=Math.max(mx-jump,0);i--)
+      {
+        this.isChecked.push(i);
+        await new Promise(resolve => setTimeout(resolve, this.dur));
+        if(this.arr[i]===num)
+        {
+          this.isFound.push(i);
+          await new Promise(resolve => setTimeout(resolve, this.dur));
+          break;
+        }
+      }
+    }
+
   //
   
 
